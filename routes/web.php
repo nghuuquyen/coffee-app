c25aa5e6eb056d63bc43082a74f70c8937b42e91
Quyen Nguyen Huu <nghuuquyen@gmail.com>
2023-12-15 13:53:28 +0700

    Implement Login / Registration / Reset Password Workflow

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,5 @@ Route::post('/checkout', [App\Http\Controllers\CheckoutController::class, 'store
 Route::get('/checkout/{order}', [App\Http\Controllers\CheckoutController::class, 'show'])->name('checkout.show');
 
 Route::get('/order-complete/{order}', App\Http\Controllers\OrderCompleteController::class)->name('orders.complete');
+
+Route::get('/dashboard', [App\Http\Controllers\DashboardController::class, 'index'])->middleware(['auth'])->name('dashboard');
